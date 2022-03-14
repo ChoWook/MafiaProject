da3afb5fd8ef2e343c131c5bede996a2681d7e7e
ChoWook <41852462+ChoWook@users.noreply.github.com>
2022-03-14 19:27:45 +0900

    Add PlayerControl
    Add PlayerControl
    Add OnMove

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : Photon.Pun.MonoBehaviourPun
+{
+    public float Speed = 5.0f;
+
+    Vector3 moveVec;
+    Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        transform.position = transform.position + moveVec * Time.fixedDeltaTime;
+    }
+
+    public void OnMove(InputValue input)
+    {
+        Vector2 inputVec = input.Get<Vector2>();
+
+        moveVec = new Vector3(inputVec.x, 0, inputVec.y);
+    }
+
+}
