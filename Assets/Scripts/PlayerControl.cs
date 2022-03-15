a35a0ffc50a58744e5ac7926e925e3007986d914
ChoWook <41852462+ChoWook@users.noreply.github.com>
2022-03-15 19:58:02 +0900

    Add OnLook
    Add OnLook

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,14 +5,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : Photon.Pun.MonoBehaviourPun
 {
-    public float Speed = 5.0f;
+    public float speedM = 5.0f;
+    public float speedH = 0.5f;
+    public float speedV = 0.5f;
 
-    Vector3 moveVec;
-    Rigidbody body;
+    private Vector2 moveInput;
+    private Vector3 moveVec;
+    private Rigidbody body;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private float movementX, movementY;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void FixedUpdate()
@@ -22,14 +30,28 @@ public class PlayerControl : Photon.Pun.MonoBehaviourPun
             return;
         }
 
+        moveVec = moveInput.x * transform.right + moveInput.y * transform.forward;
         transform.position = transform.position + moveVec * Time.fixedDeltaTime;
+    }
+
+    private void Update()
+    {
+        yaw += speedH * movementX;
+        pitch -= speedV * movementY;
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 
     public void OnMove(InputValue input)
     {
-        Vector2 inputVec = input.Get<Vector2>();
-
-        moveVec = new Vector3(inputVec.x, 0, inputVec.y);
+        moveInput = input.Get<Vector2>();
     }
 
+    public void OnLook(InputValue input)
+    {
+        Vector2 movementVector = input.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
 }
